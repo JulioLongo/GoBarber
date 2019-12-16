a68327d6d6cf072ec1e5f78f769d55c7638b4488
Julio <julio.andrade321@gmail.com>
2019-12-16 03:35:23 -0300

    Added Mail Cancellation

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '0fe53e07603189',
+    pass: '537cba60742296'
+  },
+  default: {
+    from: 'Equipe Gobarber<noreply@gobarber.com>',
+  },
+};
